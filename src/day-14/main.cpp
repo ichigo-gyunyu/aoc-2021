193bb15eacce1a0f331e93e0eec255e19b09d853
ichigo--gyuunyuu <15829692+ichigo-gyuunyuu@users.noreply.github.com>
2022-01-06 13:21:05 +0530

    day 14

--- src/day-14/main.cpp
+++ src/day-14/main.cpp
@@ -1,10 +1,13 @@
 #include <array>
 #include <chrono>
+#include <functional>
 #include <iostream>
 #include <map>
+#include <unordered_map>
 #include <vector>
 
 #define STEPS1 10
+#define STEPS2 40
 
 using std::cin;
 using std::cout;
@@ -41,7 +44,79 @@ int64_t part1(string polymer_template,
     return maxfq - minfq;
 }
 
-int64_t part2() { return 0; }
+int64_t part2(string polymer_template,
+              std::map<std::pair<char, char>, char> pair_insertions) {
+
+    std::array<int64_t, 26> fq = {0};
+
+    struct memo_obj {
+        char l;
+        char r;
+        int depth;
+
+        memo_obj(char l, char r, int depth) : l(l), r(r), depth(depth) {}
+
+        bool operator==(const memo_obj &o) const {
+            return l == o.l && r == o.r && depth == o.depth;
+        }
+    };
+    struct hashfn {
+        size_t operator()(const memo_obj &o) const {
+            auto h1 = std::hash<char>()(o.l);
+            auto h2 = std::hash<char>()(o.r);
+            auto h3 = std::hash<int>()(o.depth);
+
+            return h1 ^ h2 ^ h3;
+        }
+    };
+    std::unordered_map<memo_obj, std::array<int64_t, 26>, hashfn> memo;
+
+    std::function<std::array<int64_t, 26>(char, char, int)> f =
+        [&](char l, char r, int depth) {
+            if (depth == 0) {
+                std::array<int64_t, 26> t = {0};
+                t[l - 'A']++, t[r - 'A']++;
+                return t;
+            }
+
+            memo_obj mo(l, r, depth);
+            if (memo.contains(mo))
+                return memo[mo];
+
+            char baby = pair_insertions[{l, r}];
+            auto fq = f(l, baby, depth - 1);
+            auto fqr = f(baby, r, depth - 1);
+            for (size_t i = 0; auto e : fqr) {
+                fq[i] += e;
+                ++i;
+            }
+            fq[baby - 'A']--; // remove extra
+
+            return memo[mo] = fq;
+        };
+
+    size_t n = polymer_template.size();
+    for (size_t i = 0; i < n - 1; i++) {
+        auto t = f(polymer_template[i], polymer_template[i + 1], STEPS2);
+        for (size_t j = 0; auto e : t) {
+            fq[j] += e;
+            ++j;
+        }
+
+        if (i)
+            fq[polymer_template[i] - 'A']--;
+    }
+
+    int64_t maxfq = 0, minfq = std::numeric_limits<int64_t>::max();
+    for (auto e : fq) {
+        if (e && e < minfq)
+            minfq = e;
+        if (e > maxfq)
+            maxfq = e;
+    }
+
+    return maxfq - minfq;
+}
 
 int main() {
     string polymer_template, in1, in2;
@@ -60,7 +135,7 @@ int main() {
             .count();
 
     start = std::chrono::high_resolution_clock::now();
-    int64_t part2_ans = part2();
+    int64_t part2_ans = part2(polymer_template, pair_insertions);
     end = std::chrono::high_resolution_clock::now();
     auto time2 =
         std::chrono::duration_cast<std::chrono::milliseconds>(end - start)
